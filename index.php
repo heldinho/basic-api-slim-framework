0f8c541e69550dee7c052fac38790aa1b8472229
heldinho <hfpsis25@gmail.com>
2021-02-18 12:51:10 -0300

    fix
    Signed-off-by: heldinho <hfpsis25@gmail.com>

--- index.php
+++ index.php
@@ -6,31 +6,6 @@ require './vendor/autoload.php';
 
 $app = new \Slim\App;
 
-$app->options('/{routes:.+}', function ($request, $response, $args) {
-    return $response;
-});
-
-$app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-
-$app->add(new Tuupola\Middleware\CorsMiddleware([
-    "origin" => ["*"],
-    "methods" => ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],    
-    "headers.allow" => ["Origin", "Content-Type", "Authorization", "Accept", "ignoreLoadingBar", "X-Requested-With", "Access-Control-Allow-Origin"],
-    "headers.expose" => [],
-    "credentials" => true,
-    "cache" => 0,        
-]));
-
-/**
- * Inicio do bang :)
- * @var string
- */
 $app->post('/', function (Request $request, Response $response) use ($app) {
     $body = $request->getBody();
     $response->getBody()->write($body);
